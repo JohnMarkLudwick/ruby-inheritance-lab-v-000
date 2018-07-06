1ccdab778541b19351aa0095053ab468a21568bd
JohnMark <johnmarkludwick@gmail.com>
2018-07-06 17:23:38 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,12 @@
 class User
-  
-  attr_reader :first_name, :last_name
-  attr_writer :first_name, :last_name
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name = nil, last_name = nil)
+    @first_name = first_name
+    @last_name = last_name
+  end
+end
   
 
 end
