6ff7ccf6924450c0f8fe3f6c66ffa30eb5c4b9dc
JohnMark <johnmarkludwick@gmail.com>
2018-07-06 17:22:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,10 @@
 class Student < User
 
-  attr_accessor :knowledge
-
-
-  def initialiaze
+  def initialize
     @knowledge = []
   end
 
-
-
-
+  def learn(string)
+    knowledge << string
+  end
 end
